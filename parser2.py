5d244f7f9762829cb222b771d82b862c57de650f
LordVader23 <artem.fedotenko23@gmail.com>
2020-09-09 17:43:55 +0300

    Added pagination

--- parser2.py
+++ parser2.py
@@ -45,8 +45,60 @@ class Bot2(Bot):
         # search_button.click()
         self.driver.execute_script("arguments[0].click();", search_button)
 
+        self.get_trademarks()
+
     def get_trademarks(self):
-        
+        self.wait_loader()  # To wait until page is loaded
+
+        brands = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_BRAND']")
+        sources = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_SOURCE']")
+        statuses = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_STATUS']")
+        relevans = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_score']")
+        origins = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_OO']")
+        holders = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_HOL']")
+        holder_countries = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_HOLC']")
+        nums = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_ID']")
+        app_dates = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_AD']")
+        img_classes = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_LOGO']")
+        nices = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_NC']")
+        imgs = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_IMG']")
+        links = self.driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_IMG'] img")
+
+        arr = zip(brands, sources, statuses, relevans, origins, holders, holder_countries,
+                  nums, app_dates, img_classes, nices, imgs)
+        data = []
+
+        for elem in arr:
+            data_row = []
+
+            for i in elem[:-1]:
+                if i.text == ' ':
+                    data_row.append('')
+                else:
+                    data_row.append(i.text)
+
+            if elem[-1].text == ' ':
+                data_row.append('')
+            else:
+                data_row.append(links[0].get_attribute('src'))
+                del links[0]
+
+            data.append(data_row)
+
+        for i in data:
+            print(i)
+        self.write_csv(data)
+        # To paginate
+        try:
+            self.wait_more(wait2, EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                              "a[aria-label='next page']")[0]))
+
+            next_page_button = self.driver.find_elements_by_css_selector("a[aria-label='next page']")
+            next_page_button[0].click()
+        except Exception:
+            return None
+
+        self.get_trademarks()
 
     def wait_loader(self):
         while True:
