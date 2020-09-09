2886cf949f4ef50ffe587cbff7242afdb71d580d
LordVader23 <artem.fedotenko23@gmail.com>
2020-09-09 16:50:23 +0300

    Fixed bug with search button

--- parser2.py
+++ parser2.py
@@ -17,6 +17,7 @@ class Bot2(Bot):
 
     def __init__(self):
         super().__init__()
+        self.driver.set_window_size(1024, 720)
 
     def run(self):
         wait2 = WebDriverWait(self.driver, 15)
@@ -25,25 +26,35 @@ class Bot2(Bot):
 
         self.wait_loader()
 
-        country_button = self.driver.find_element_by_css_selector("a['href'='#country_search']")
+        country_button = self.driver.find_element_by_css_selector("a[href='#country_search']")
         country_button.click()
 
         designation_input = self.driver.find_element_by_css_selector("input#DS_input")
         designation_input.send_keys('KZ')
 
+        sleep(3)
+        ul = self.driver.find_element_by_css_selector("ul#ui-id-26")
+        ul.click()
+
+        # some_div = self.driver.find_element_by_css_selector("div.desc")
+        # some_div.click()
+
+        sleep(5)
         search_button = self.driver.find_element_by_css_selector(
                                                         "div.searchButtonContainer.bottom.right a span.ui-button-text")
-        search_button.click()
+        # search_button.click()
+        self.driver.execute_script("arguments[0].click();", search_button)
 
     def get_trademarks(self):
-        pass
+        
 
     def wait_loader(self):
         while True:
             sleep(3)
 
             try:
-                loader = self.driver.find_element_by_css_selector("div.pagerAjaxIcon.ajaxIcon")
+                loader = self.driver.find_element_by_css_selector(
+                    "div.results_navigation.top_results_navigation.displayButtons.hover div.pagerAjaxIcon.ajaxIcon")
             except NoSuchElementException:
                 break
 
